507dbf0e9766f508c058c0aea4ad0a438a1df3ab
Marcus Crane <marcus@thingsima.de>
2017-10-14 20:54:11 +1300

    A tiny hiragana project but oh no, Fargo is on TV so i'll do it later

---
+++ hiragana/hiragana.py
@@ -0,0 +1,22 @@
+import json
+
+file = open('hiragana.json', 'r')
+hiragana = json.loads(file.read())
+agroup = hiragana['a']
+
+print('Identify the following:')
+results = { 'y': 0, 'n': 0 }
+
+for key in agroup:
+  print(agroup[key])
+
+  answer = input('> ')
+
+  if answer == key:
+    results['y'] += 1
+    print('Correct!')
+  else:
+    results['n'] += 1
+    print('Sorry, that was actually the letter {0}'.format(key))
+
+print('You got {0}/{1} correct!'.format(results['y'], len(agroup)))

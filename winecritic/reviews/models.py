d65effc9366060461edd4f921f73c0585e36fb6f
Marcus Crane <marcus@thingsima.de>
2017-10-18 17:54:49 +1300

    Added a model for wine and vintages

---
+++ winecritic/reviews/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Wine(models.Model):
+  '''Define a wine model which can contain multiple vintages ()'''
+  # I'm going to leave out a lot of types for the sake of readability
+  TYPES = (
+    ('CH', 'Chardonnay'),
+    ('PB', 'Pinot Blanc'),
+    ('PG', 'Pinot Gris'),
+    ('RS', 'Riesling'),
+    ('SB', 'Sauvignon Blanc'),
+    ('SG', 'Sauvignon Gris'),
+    ('ME', 'Merlot'),
+    ('PN', 'Pinot Noir'),
+  )
+  name = models.CharField(max_length=200)
+  type = models.CharField(max_length=50)
+  price = models.IntegerField()
+
+class Vintage(models.Model):
+  '''Define a vintage, of which there is usually one per year'''
+  wine = models.ForeignKey(Wine, on_delete=models.CASCADE)
+  year = models.DateField('year produced')
+  rating = models.IntegerField(default=0)

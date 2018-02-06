e4de997cd6d0dfc9500943f82e2c592401ffc829
Marcus Crane <marcus@thingsima.de>
2018-02-06 19:14:03 +1300

    Generated Python and C# dirs

--- exercism/csharp/hello-world/HelloWorld.cs
+++ exercism/csharp/hello-world/HelloWorld.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
-public static class HelloWorld
+public class HelloWorld
 {
     public static string Hello()
     {
-        throw new NotImplementedException("You need to implement this function.");
+    	return "Hello, World!";
     }
 }
